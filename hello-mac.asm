12c39663ca36a4f42b0d379646dab1f12c30603c
João Magalhães <joamag@gmail.com>
2020-12-12 20:00:48 +0000

    Small fixes for mac

--- hello-mac.asm
+++ hello-mac.asm
@@ -1,6 +1,7 @@
 section .data
     hello:      db      "Hello, world!", 10
     hello.len:  equ     $ - hello
+    counter:    dd      10h
 
 section .text
     global _start
@@ -11,6 +12,10 @@ _start:
     mov rsi, hello
     mov rdx, hello.len
     syscall
+    mov ecx, [counter]   ; moves the counter to ECX
+    dec ecx              ; decrements ECX by one
+    mov [counter], ecx   ; re-stores the ECX back to memory
+    jnz _start           ; jumps in case the counter is not yet zero
 
     mov rax, 0x2000001   ; 'exit' system call
     mov rdi, 0
