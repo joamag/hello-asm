a2d06cf867d9c759364716a0205f6c4d5684f10f
João Magalhães <joamag@gmail.com>
2020-12-12 22:49:48 +0000

    Added new comments

--- hello-windows.asm
+++ hello-windows.asm
@@ -10,8 +10,8 @@ main:
     sub rsp, 40 
 
 loop:
-    mov rcx, hello
-    call printf
+    mov rcx, hello             ; sets the hello string pointer as first argument (uses 64 bit calling convetion)
+    call printf                ; calls the 'printf' function
     mov ecx, [counter]         ; moves the counter to ECX
     dec ecx                    ; decrements ECX by one
     mov [counter], ecx         ; re-stores the ECX back to memory
