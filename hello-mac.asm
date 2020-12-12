3c4317b028d4236a271501707b29754f90f19f00
João Magalhães <joamag@gmail.com>
2020-12-12 20:10:51 +0000

    Small fixes

--- hello-mac.asm
+++ hello-mac.asm
@@ -1,22 +1,22 @@
 section .data
     hello:      db      "Hello, world!", 10
     hello.len:  equ     $ - hello
-    counter:    dd      10h
+    counter:    dq      10h
 
 section .text
     global _start
 
 _start:
-    mov rax, 0x2000004   ; 'write' system call
-    mov rdi, 1           ; file descriptor 1 = STDOUT
+    mov rax, 0x2000004         ; 'write' system call
+    mov rdi, 1                 ; file descriptor 1 = STDOUT
     mov rsi, hello
     mov rdx, hello.len
     syscall
-    mov ecx, [counter]   ; moves the counter to ECX
-    dec ecx              ; decrements ECX by one
-    mov [counter], ecx   ; re-stores the ECX back to memory
-    jnz _start           ; jumps in case the counter is not yet zero
+    mov rsi, [rel counter]     ; moves the counter to ECX
+    dec rsi                    ; decrements ECX by one
+    mov [rel counter], rsi     ; re-stores the ECX back to memory
+    jnz _start                 ; jumps in case the counter is not yet zero
 
-    mov rax, 0x2000001   ; 'exit' system call
+    mov rax, 0x2000001         ; 'exit' system call
     mov rdi, 0
     syscall
--- hello.asm
+++ hello.asm
@@ -7,16 +7,16 @@ section .text
     global _start
 
 _start:
-    mov eax, 4           ; 'write' system call = 4
-    mov ebx, 1           ; file descriptor 1 = STDOUT
-    mov ecx, hello       ; string to write
-    mov edx, hello.len   ; length of string to write
-    int 80h              ; call the kernel (syscall) using 0x80 interrupt
-    mov ecx, [counter]   ; moves the counter to ECX
-    dec ecx              ; decrements ECX by one
-    mov [counter], ecx   ; re-stores the ECX back to memory
-    jnz _start           ; jumps in case the counter is not yet zero
+    mov eax, 4                 ; 'write' system call = 4
+    mov ebx, 1                 ; file descriptor 1 = STDOUT
+    mov ecx, hello             ; string to write
+    mov edx, hello.len         ; length of string to write
+    int 80h                    ; call the kernel (syscall) using 0x80 interrupt
+    mov ecx, [counter]         ; moves the counter to ECX
+    dec ecx                    ; decrements ECX by one
+    mov [counter], ecx         ; re-stores the ECX back to memory
+    jnz _start                 ; jumps in case the counter is not yet zero
 
-    mov eax, 1           ; 'exit' system call
-    mov ebx, 0           ; exit with error code 0
-    int 80h              ; call the kernel (syscall) using 0x80 interrupt
+    mov eax, 1                 ; 'exit' system call
+    mov ebx, 0                 ; exit with error code 0
+    int 80h                    ; call the kernel (syscall) using 0x80 interrupt
