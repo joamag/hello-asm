1dfc6c6b64c995e6174a5a3899b6719699a64fc4
João Magalhães <joamag@gmail.com>
2020-12-12 19:34:32 +0000

    Small fixes

--- hello.asm
+++ hello.asm
@@ -10,8 +10,8 @@ _start:
     mov ebx, 1            ; file descriptor 1 = STDOUT
     mov ecx, hello        ; string to write
     mov edx, hello.len    ; length of string to write
-    int 80h               ; call the kernel (syscal)
+    int 80h               ; call the kernel (syscall) using 0x80 interrupt
 
     mov eax,1            ; 'exit' system call
     mov ebx,0            ; exit with error code 0
-    int 80h              ; call the kernel (syscal)
+    int 80h              ; call the kernel (syscall) using 0x80 interrupt
