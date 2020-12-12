04be7f659e559e4d6cf4e8fc65c0dd1626dcad02
João Magalhães <joamag@gmail.com>
2020-12-12 19:36:17 +0000

    Small comment fixes

--- hello.asm
+++ hello.asm
@@ -6,11 +6,11 @@ section .text
     global _start 
 
 _start:
-    mov eax, 4            ; 'write' system call = 4
-    mov ebx, 1            ; file descriptor 1 = STDOUT
-    mov ecx, hello        ; string to write
-    mov edx, hello.len    ; length of string to write
-    int 80h               ; call the kernel (syscall) using 0x80 interrupt
+    mov eax, 4           ; 'write' system call = 4
+    mov ebx, 1           ; file descriptor 1 = STDOUT
+    mov ecx, hello       ; string to write
+    mov edx, hello.len   ; length of string to write
+    int 80h              ; call the kernel (syscall) using 0x80 interrupt
 
     mov eax,1            ; 'exit' system call
     mov ebx,0            ; exit with error code 0
