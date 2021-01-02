7b379d876194cebaf6ecd9ed58ae3c5bbbf05121
João Magalhães <joamag@gmail.com>
2021-01-02 20:12:35 +0000

    fix: made windows version use puts

--- hello-windows.asm
+++ hello-windows.asm
@@ -1,24 +1,24 @@
 section .data
-    hello:      db      "Hello, world!", 10, 0
+    hello:      db      "Hello, world!", 0
     counter:    dd      10
 
 section .text
     global main
-    extern printf
+    extern puts
 
 main:
-    sub rsp, 40
+    sub rsp, 28h
 
 loop:
     mov rcx, hello             ; sets the hello string pointer as first argument (uses 64 bit __fastcall calling convention)
                                ; for more information check https://en.wikipedia.org/wiki/X86_calling_conventions or the
                                ; original Microsoft documentation at https://docs.microsoft.com/en-us/cpp/build/x64-calling-convention
-    call printf                ; calls the 'printf' function
-    mov ecx, [counter]         ; moves the counter to ECX
-    dec ecx                    ; decrements ECX by one
-    mov [counter], ecx         ; re-stores the ECX back to memory
+    call puts                  ; calls the 'puts' function
+    mov rsi, [counter]         ; moves the counter to RSI
+    dec rsi                    ; decrements RSI by one
+    mov [counter], rsi         ; re-stores the RSI back to memory
     jnz loop                   ; jumps in case the counter is not yet zero
 
 exit:
-    add rsp, 40
+    add rsp, 28h
     ret
